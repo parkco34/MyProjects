33bb0e7d23d7a5ed1df6039ac0592a1850de70bc
Parker <parkercorya@gmail.com>
2019-09-27 15:22:22 -0400

    Initial project version

---
+++ MyBankStuff.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+dfmnth = pd.read_csv('C:/Users/parkd/Desktop/Excel/AUG_SEPT.csv')
+locat = dfmnth.loc[dfmnth['']]
+
+
+
+# =============================================================================
+# write = pd.ExcelWriter('C:/Users/parkd/Desktop/Excel/AugToSept.xlsx')
+# df.to_excel(write,index=False)
+# write.close()
+# =============================================================================
+
+
+
+
+    
+
