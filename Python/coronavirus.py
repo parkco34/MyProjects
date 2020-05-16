83b5162e7ac3f446e084ca614f38ce9fda388f3b
ParkDaddy <parkercorya@gmail.com>
2020-05-15 20:11:20 -0400

    rona

--- Python/coronavirus.py
+++ Python/coronavirus.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import datetime as dt
+from datetime import date, timedelta, datetime
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -11,48 +11,47 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from io import StringIO
 import re
+import difflib
 
-start_date = '02-24-2020'
-final_date = '03-25-2020'
-today = dt.datetime.now()
-tdy = today.strftime('%m-%d-%Y')
+start_date = '04-26-2020'
+today = date.today()
+yesterday = today - timedelta(days=1)
+yesterday = yesterday.strftime('%m-%d-%Y')
 
-def get_dateinterval(start_date, final_date):
-    start_date = dt.datetime.strptime(start_date, '%m-%d-%Y')
-    final_date = dt.datetime.strptime(final_date, '%m-%d-%Y')
-    dates = pd.date_range(start_date, final_date, freq='d')
-    dates = dates.strftime('%m-%d-%Y')
-    dates = dates.tolist()
-    return dates
 # =============================================================================
-# def get_dates(start_date):
-#     start_date = dt.datetime.strptime(start_date, '%m-%d-%Y')
-#     dates = pd.date_range(start_date, today - dt.timedelta(days=1),freq='d')
+# def get_dateinterval(start_date, final_date):
+#     start_date = strptime(start_date, '%m-%d-%Y')
+#     final_date = strptime(yesterday, '%m-%d-%Y')
+#     dates = pd.date_range(start_date, final_date, freq='d')
 #     dates = dates.strftime('%m-%d-%Y')
 #     dates = dates.tolist()
 #     return dates
 # =============================================================================
+def get_dates(start_date):
+    start_date = datetime.strptime(start_date, '%m-%d-%Y')
+    dates = pd.date_range(start_date, today - timedelta(days=1), freq='d')
+    dates = dates.strftime('%m-%d-%Y')
+    dates = dates.tolist()
+    return dates
 
-# Webscraping /\/\/\/\/\/\/\/\/\/\/\/\/\/\//\/\/\/\/\/\/\/\/\/\/\/\/\/\//\/\/\/\/\/\/\/\/\/\/\/\/\/\//\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+def replace_cols(df, new_columns):
+    k = 0
+    for i in df.columns:
+        for j in column_names:
+            seq = difflib.SequenceMatcher(None,i, j).ratio()*100
+            if seq >= 85:
+                newcol = re.sub(i, j, i)
+                df.columns.values[k] = newcol
+#                print(newcol)
+                k += 1
+
 options = Options()
 options.add_argument("--start-maximized")
 options.headless = True
 assert options.headless
 
-pattern_list = ['Province/State','Country/Region','Last Update','Confirmed','Deaths','Recovered']
-def replace_cols(df, pattern, string):
-    k = 0
-    for i in df.columns:
-        newcol = re.sub(pattern, string, i)
-        df.columns.values[k] = newcol
-        print(newcol)
-        k += 1
-
-# Create empty dataframe:
 column_names = ['FIPS','Admin2','Province_State','Country_Region','Last_Update','Lat','Long_','Confirmed','Deaths','Recovered','Active','Combined_Key']
-df = pd.DataFrame(columns=column_names)
 
-#def scrape(start_date, final_date, path_to_click):
 def scrape(start_date, final_date, path_to_click):
     chrome_driver = "C:/Users/parkd/MyScripts/chromedriver.exe"
     site = "https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports"
@@ -61,30 +60,32 @@ def scrape(start_date, final_date, path_to_click):
     
     waiting = WebDriverWait(driver, 20)
     
-#    for date in get_dates(start_date):
-    for date in get_dateinterval(start_date, final_date):
-        waiting.until(EC.element_to_be_clickable((By.XPATH, '//*[@title="{}.csv"]'.format(date)))).click()
+    df = pd.DataFrame(columns=column_names)
+
+    for dt in get_dates(start_date):
+        waiting.until(EC.element_to_be_clickable((By.XPATH, '//*[@title="{}.csv"]'.format(dt)))).click()
         waiting.until(EC.element_to_be_clickable((By.XPATH, path_to_click))).click()
-#        driver.find_element_by_xpath('/html/body/div[4]/div/main/div[2]/div/div[3]/div[1]/div[2]/div[1]/a[1]').click()
         raw = driver.find_element_by_xpath('/html/body/pre').text
         raw_data = StringIO(raw)
         
-        if date == '02-24-2020':
- #       if date == '05-01-2020':
+        if date == start_date:
             df1 = pd.read_csv(raw_data)
-            
-            rona_info = pd.concat([df, df1], axis=0, ignore_index=True)
+            replace_cols(df1, column_names)
+            df1['Last_Update'] = pd.to_datetime(df1['Last_Update'])
+            df = pd.concat([df, df1], axis=0, ignore_index=True)
         else:
             df2 = pd.read_csv(raw_data)
-            rona_info = pd.concat([df1, df2], axis=0, ignore_index=True)
+            replace_cols(df2, column_names)
+            df2['Last_Update'] = pd.to_datetime(df2['Last_Update'])
+            df = pd.concat([df, df2], axis=0, ignore_index=True)
 
         driver.back()
         driver.back()    
 
-    return rona_info
+    return df
 
-rona = scrape('02-24-2020', '03-25-2020', '/html/body/div[4]/div/main/div[2]/div/div[3]/div[1]/div[2]/div[1]/a[1]').to_csv('C:/Users/parkd/MyScripts/raw_data/Coronavirus_update {}.csv'.format(final_date), index=False)
-#rona = scrape('05-01-2020', '/html/body/div[4]/div/main/div[2]/div/div[3]/div[1]/div[2]/div[1]/a[1]')
+rona = scrape('04-26-2020', yesterday, '/html/body/div[4]/div/main/div[2]/div/div[3]/div[1]/div[2]/div[1]/a[1]')
+#rona.to_csv('C:/Users/parkd/MyScripts/raw_data/Coronavirus_update {}.csv'.format(yesterday), index=False)
 
 #df = pd.read_csv(r'C:\Users\parkd\MyScripts\raw_data\Coronavirus_update 04-30-2020.csv')
 # Setting datetimeIndex:
@@ -97,10 +98,6 @@ rona = scrape('02-24-2020', '03-25-2020', '/html/body/div[4]/div/main/div[2]/div
 # =============================================================================
 
 # MAKE DAILY AUTOMATIC: /\/\/\/\/\/\/\/\/\/\/\/\/\/\//\/\/\/\/\/\/\/\/\/\/\/\/\/\//\/\/\/\/\/\/\/\/\/\/\/\/\/\//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
-
-#df = pd.concat([df, rona], axis=0, ignore_index=True)
-# Data Analysis: /\/\/\/\/\/\/\/\/\/\/\/\/\/\//\/\/\/\/\/\/\/\/\/\/\/\/\/\//\/\/\/\/\/\/\/\/\/\/\/\/\/\//\/\/\/\/\/\/\/\/\/\/\/\/\/\/
-
 
 
 # Send Email: /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
